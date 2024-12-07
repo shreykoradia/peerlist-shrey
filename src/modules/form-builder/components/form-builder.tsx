26304590dab8e8a615ec92ffeaf1b2135994fadb
Shrey Koradia <shrey.radicalloop@gmail.com>
2024-12-07 19:13:15 +0530

    added footer for form-builder

--- src/modules/form-builder/components/form-builder.tsx
+++ src/modules/form-builder/components/form-builder.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import FormHeader from "./form-header";
+import FormFooter from "./form-footer";
 
 function FormBuilder() {
   return (
     <div className="mx-auto flex flex-col justify-center border-y-0 border-x border-x-secondary-foreground max-w-screen-sm">
       <FormHeader />
       <div className="h-[calc(100vh_-_7.5rem)]"></div>
-      <div>Footer</div>
+      <FormFooter />
     </div>
   );
 }
--- src/modules/form-builder/components/form-footer.tsx
+++ src/modules/form-builder/components/form-footer.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
+import { Button } from "@/shared/ui/button";
+
+import DraftIcon from "@/assets/icons/draft.svg";
+import CheckIcon from "@/assets/icons/check.svg";
+
 function FormFooter() {
-  return <div>form-footer</div>;
+  return (
+    <>
+      <div className="w-full flex justify-between items-center border-t border-t-secondary-foreground px-6 py-4 bg-secondary">
+        <Button variant={"outline"} className="gap-1">
+          <DraftIcon />
+          Save as draft
+        </Button>
+        <Button variant={"default"} className="gap-1">
+          <CheckIcon className={"mt-0.5"} />
+          Publish form
+        </Button>
+      </div>
+    </>
+  );
 }
 
 export default FormFooter;
