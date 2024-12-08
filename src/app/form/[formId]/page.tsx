92561c02be99aea1d98cbd41a97f86a0ef534654
Shrey Koradia <shrey.radicalloop@gmail.com>
2024-12-09 02:43:48 +0530

    fixed build issues

--- src/app/form/[formId]/page.tsx
+++ src/app/form/[formId]/page.tsx
@@ -1,13 +1,7 @@
 import FormPreview from "@/modules/form-viewer";
 import Container from "@/shared/ui/container";
 
-export default function FormViewPage({
-  params,
-  searchParams,
-}: {
-  params: { formId: string };
-  searchParams?: { [key: string]: string };
-}) {
+export default function FormViewPage() {
   return (
     <Container>
       <FormPreview />
